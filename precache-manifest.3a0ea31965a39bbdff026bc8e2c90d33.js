cf3113f4fb78169e999336c232eccf43fbcba7d7
Phryxia <xahhaepica@gmail.com>
2020-01-29 21:11:16 +0900

    Updates

---
+++ precache-manifest.3a0ea31965a39bbdff026bc8e2c90d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be389acc1de5624d90efced00afb2117",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "fafaccc83a7a3d6752ff",
+    "url": "/react-calc/static/css/main.4ea39941.chunk.css"
+  },
+  {
+    "revision": "1a8f13d8f4b0bc3dc289",
+    "url": "/react-calc/static/js/2.050b01fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calc/static/js/2.050b01fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fafaccc83a7a3d6752ff",
+    "url": "/react-calc/static/js/main.40f80289.chunk.js"
+  },
+  {
+    "revision": "dc85cc2f4f16a1535458",
+    "url": "/react-calc/static/js/runtime-main.db3bf958.js"
+  }
+]);
